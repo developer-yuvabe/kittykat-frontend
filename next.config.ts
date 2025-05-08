0f233d367f4d7a8d7696cead2602e1ed54616cbf
arun-j0 <arun2310kumar2002@gmail.com>
2025-05-08 16:09:24 +0530

    feat: update next.config.ts to include image remote patterns and maintain ESLint settings

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,25 @@
-import type { NextConfig } from "next";
+const nextConfig = {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  output: "standalone",
 
-const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "storage.googleapis.com",
+        pathname: "/**", // Allow all paths under storage.googleapis.com
+      },
+      {
+        protocol: "https",
+        hostname: "replicate.delivery",
+        pathname: "/**", // Allow all paths under storage.googleapis.com
+      },
+    ],
+  },
 };
 
 export default nextConfig;
