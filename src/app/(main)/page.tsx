39f5cc562a27098696ac2591556b2bd6471a848e
arun-j0 <arun2310kumar2002@gmail.com>
2025-05-12 09:16:13 +0530

    refactor: remove last interacted brand ID logic from Page component

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -8,23 +8,11 @@ import { useUserStore } from "@/store/user.store";
 import { useEffect } from "react";
 
 export default function Page() {
-  const lastInteractedBrandId = useUserStore((state) =>
-    state.getLastInteractedBrandId()
-  );
-
-  useEffect(() => {
-    if (lastInteractedBrandId) {
-      // Replace this with the actual logic to send the brand ID
-      console.log("Sending last interacted brand ID:", lastInteractedBrandId);
-      // e.g., fetch or socket message
-    }
-  }, [lastInteractedBrandId]);
-
   return (
     <div className="bg-white">
       <ThreadProvider>
         <StreamProvider>
-          <Thread brandId={lastInteractedBrandId} />
+          <Thread brandId={null} />
         </StreamProvider>
       </ThreadProvider>
     </div>
