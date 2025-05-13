ea418834947ba254021bd17a79aaf73627c7f0cb
Dhanush <iamdhanush02@gmail.com>
2025-05-13 10:33:25 +0530

    fix: add margin-top to Page component and border-bottom to TopNavigation to enhace merge visibility

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -6,7 +6,7 @@ import { ThreadProvider } from "@/providers/Thread";
 
 export default function Page() {
   return (
-    <div className="bg-white">
+    <div className="bg-white mt-2">
       <ThreadProvider>
         <StreamProvider>
           <Thread brandId={null} />
--- src/components/shared/TopNavigation.tsx
+++ src/components/shared/TopNavigation.tsx
@@ -4,7 +4,7 @@ import { UserProfileMenu } from "./UserProfileMenu";
 
 export function TopNavigation() {
   return (
-    <div className="w-full h-24 px-4 flex items-center justify-between  border-[#f3f4f6] bg-white sticky top-0 z-10">
+    <div className="w-full h-24 px-4 flex items-center justify-between  border-[#f3f4f6] bg-white sticky top-0 z-10 border-b">
       <LogoSection />
       <NavLinks />
       <UserProfileMenu />
