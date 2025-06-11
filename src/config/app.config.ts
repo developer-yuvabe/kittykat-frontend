4342c762a54861737c39c735cb4f453d5b7daf03
logi-22 <logilogu1822@gmail.com>
2025-06-11 15:42:50 +0530

    feat: Implement Forgot Password functionality with email and reset password forms

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -3,7 +3,7 @@ import { env } from "./env";
 export const AppConfig = {
   APP_NAME: "KittyKat",
 
-  PUBLIC_PATHS: ["/login", /^\/invitations\/[^\/]+$/],
+  PUBLIC_PATHS: ["/login","/forgot-password",/^\/invitations\/[^\/]+$/],
   HOME_ROUTE: "/",
   BASE_URLS: {
     prod: env.NEXT_PUBLIC_API_BASE_URL_PROD,
