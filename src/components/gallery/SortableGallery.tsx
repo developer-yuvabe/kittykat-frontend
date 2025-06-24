0d97118d85cb178d4a43e0f86aeabd4b0a029036
arun-j0 <arun2310kumar2002@gmail.com>
2025-06-24 10:13:17 +0530

    feat: Enhance SortableGallery with gradient overlays and improved icon visibility on hover

--- src/components/gallery/SortableGallery.tsx
+++ src/components/gallery/SortableGallery.tsx
@@ -143,7 +143,7 @@ export default function SortableGallery<
     const sortablePhoto = photo as SortablePhoto<TPhoto>;
     return (
       <Sortable key={sortablePhoto.id} id={sortablePhoto.id}>
-        <Component {...props} />
+        <Component {...props} className="relative group" />
       </Sortable>
     );
   };
@@ -157,7 +157,7 @@ export default function SortableGallery<
         collisionDetection={closestCenter}
       >
         <SortableContext items={photos}>
-          <div className={classes.gallery}>
+          <div className={`${classes.gallery} `}>
             <Gallery
               ref={ref}
               photos={photos}
@@ -175,7 +175,10 @@ export default function SortableGallery<
                   return (
                     <>
                       {/* Delete Icon (top-left) */}
-                      <div className="absolute top-2 left-2 z-10">
+                      {/* Gradient overlays */}
+                      <div className="absolute top-0 left-0 right-0 h-1/5 bg-gradient-to-b from-black/40 to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-200 pointer-events-none" />
+                      <div className="absolute bottom-0 left-0 right-0 h-1/5 bg-gradient-to-t from-black/40 to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-200 pointer-events-none" />
+                      <div className="absolute top-2 left-2 z-10 opacity-0 group-hover:opacity-100 transition-opacity duration-200">
                         <X
                           size={16}
                           className={`w-5 h-5 cursor-pointer transition-colors text-white fill-white hover:scale-110 active:scale-95`}
@@ -188,7 +191,7 @@ export default function SortableGallery<
                       </div>
 
                       {/* Expand Icon (top-right) */}
-                      <div className="absolute top-2 right-2 z-10">
+                      <div className="absolute top-2 right-2 z-10  opacity-0 group-hover:opacity-100 transition-opacity duration-200">
                         <Maximize2
                           size={16}
                           className="w-5 h-5 cursor-pointer transition-colors text-white hover:scale-110 active:scale-95"
@@ -202,7 +205,7 @@ export default function SortableGallery<
 
                       {/* Regenerate Icon (bottom-left) */}
                       {!hasUnsavedChanges && (
-                        <div className="absolute bottom-2 left-2 z-10">
+                        <div className="absolute bottom-2 left-2 z-10  opacity-0 group-hover:opacity-100 transition-opacity duration-200">
                           <RotateCcw
                             size={16}
                             className="w-5 h-5 cursor-pointer transition-colors text-white hover:scale-110 active:scale-95"
@@ -220,7 +223,7 @@ export default function SortableGallery<
 
                       {/* Like Icon (bottom-right) - only show if onPhotoLike is provided */}
                       {onPhotoLike && (
-                        <div className="absolute bottom-2 right-2 z-10">
+                        <div className="absolute bottom-2 right-2 z-10  opacity-0 group-hover:opacity-100 transition-opacity duration-200">
                           <Heart
                             size={16}
                             className={`w-5 h-5 cursor-pointer transition-colors ${
