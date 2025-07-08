6d07a4ccb0b6453e75f54c3b023a1a8af8337d1b
arun-j0 <arun2310kumar2002@gmail.com>
2025-07-08 10:28:48 +0530

    fix : style issue in vton image fixed

--- src/components/chatbot/a2i/features/VirtualTryOn.tsx
+++ src/components/chatbot/a2i/features/VirtualTryOn.tsx
@@ -1,7 +1,7 @@
 import { Button } from "@/components/ui/button";
 import { UploadIcon } from "@/components/ui/custom-icon";
 import { BrainIcon, X } from "lucide-react";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { cn, delay } from "@/lib/utils";
 import { createVtonImage } from "@/services/api/vton.service";
 import { useBrandStore } from "@/store/brand.store";
@@ -23,6 +23,12 @@ const VirtualTryOn = ({
   const [garmentImage, setGarmentImage] = useState<string | null>(null);
   const [showMediaLibrary, setShowMediaLibrary] = useState(false);
   const [loading, setLoading] = useState(false);
+  console.log("render");
+  console.log("vton", productImage);
+
+  useEffect(() => {
+    console.log("viton", garmentImage);
+  }, [garmentImage]);
 
   const handelVtonGeneration = async () => {
     if (!garmentImage) {
@@ -61,12 +67,17 @@ const VirtualTryOn = ({
         }}
       >
         {garmentImage ? (
-          <div className="absolute inset-0">
+          <div className="flex items-center justify-center">
             <img
               src={garmentImage}
               alt="Garment"
-              className="object-contain w-full h-full"
+              className="object-contain w-[70%] h-[400px] lg:h-[450px] xl:h-[600px]"
+              onLoad={() => console.log("Garment image loaded")}
+              onError={() =>
+                console.error("Failed to load garment image:", garmentImage)
+              }
             />
+
             <Button
               disabled={loading}
               onClick={(e) => {
@@ -76,7 +87,6 @@ const VirtualTryOn = ({
               className="bg-destructive/10 text-destructive border-destructive border border-dashed hover:bg-destructive/15 absolute top-2 right-2 z-[1000]"
             >
               <X />
-              Remove garment
             </Button>
           </div>
         ) : (
@@ -105,7 +115,8 @@ const VirtualTryOn = ({
           }
         }}
         onMediaItemSelected={(mediaItem) => {
-          setGarmentImage(mediaItem);
+          console.log("selected mediaItem:", mediaItem);
+          setGarmentImage(mediaItem); // should be a valid URL string
           setShowMediaLibrary(false);
         }}
       />
