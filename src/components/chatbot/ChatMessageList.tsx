207e3f4acd9a28214319070fbd573b8463c6a329
arun-j0 <arun2310kumar2002@gmail.com>
2025-07-21 14:18:19 +0530

    feat : display specific status in agent chat loader instead of generic messages for tool call

--- src/components/chatbot/ChatMessageList.tsx
+++ src/components/chatbot/ChatMessageList.tsx
@@ -1,13 +1,11 @@
 import { Checkpoint } from "@langchain/langgraph-sdk";
 import React, { Dispatch, SetStateAction, useCallback, useMemo } from "react";
 import { HumanMessage } from "../thread/messages/human";
-import {
-  AssistantMessage,
-  AssistantMessageLoading,
-} from "../thread/messages/ai";
+import { AssistantMessage } from "../thread/messages/ai";
 import { DO_NOT_RENDER_ID_PREFIX } from "@/lib/constants";
 import { useStreamContext } from "@/providers/langgraph/Stream";
 import { parseAsBoolean, useQueryState } from "nuqs";
+import { AssistantMessageLoader } from "../thread/messages/AssistantMessageLoader";
 
 type ChatMessageListProps = {
   firstTokenReceived: boolean;
@@ -85,7 +83,7 @@ export const ChatMessageList: React.FC<ChatMessageListProps> = ({
         />
       )}
 
-      {isLoading && !firstTokenReceived && <AssistantMessageLoading />}
+      {isLoading && !firstTokenReceived && <AssistantMessageLoader />}
 
       {agentIndicator}
     </>
