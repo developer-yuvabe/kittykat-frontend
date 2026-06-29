58f3f70052b4c94e734e59eab11c1167eff97583
Abineshkumar <abineshkumar@yuvabe.com>
2026-06-29 07:19:38 +0530

    refactor: remove dynamic export declaration from layout component

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -4,8 +4,6 @@ import MainLayout from "./_components/MainLayout";
 import { redirect } from "next/navigation";
 import Splash from "@/components/shared/Splash";
 
-export const dynamic = "force-dynamic";
-
 const layout = async ({ children }: { children: React.ReactNode }) => {
   const { error, user } = await fetchUser();
 
